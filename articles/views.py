f70918baf1fd284f7d954476b24975ba06eeca89
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-08-18 20:30:13 +0300

    версия 1.3

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-from articles.models import Article, Thematic
+from articles.models import Article, Thematic, Membership
 
 
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
-    context = {'object_list': Article.objects.all().order_by(ordering), 'tags': Thematic.objects.all()}
-
+    context = {'object_list': Article.objects.all().order_by(ordering), 'tags': Thematic.objects.all(),
+               'membership': Membership.objects.all()}
     return render(request, template, context)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zwerg/Рабочий стол/Python_progect/News_site/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
